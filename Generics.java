6d16e4377fda39ed4b33fe9fb7433bcfbc2e676a
sandrine <poupardsandrine@gmail.com>
2019-01-23 15:28:49 +0100

    quest finished

--- Generics.java
+++ Generics.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -5,87 +6,102 @@ import java.util.List;
 
 public class Generics {
 
-  interface Predicate<T> {
+    interface Predicate<T> {
 
-    boolean filter(T obj);
-  }
+        boolean filter(T obj);
+    }
 
-  interface Mapper<T, U> {
+    interface Mapper<T, U> {
 
-    U map(T obj);
-  }
+        U map(T obj);
+    }
 
-  /**
-   * Java entry point
-   */
-  public static void main(String... args) {
+    /**
+     * Java entry point
+     */
+    public static void main(String... args) {
 
-      new Generics();
+        new Generics();
 
-  }
+    }
 
-  /**
-   * Class Constructor
-   */
-  private Generics() {
+    /**
+     * Class Constructor
+     */
+    private Generics() {
 
-    List<Integer> ints = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-    List<String>  strs = Arrays.asList("tout", "titi", "ototo", "jean", "tous", "taratata");
+        List<Integer> ints = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<String> strs = Arrays.asList("tout", "titi", "ototo", "jean", "tous", "taratata");
 
-    //  Ne retourner que les nombres pairs.
-    ints = filter(ints, i -> ( i % 2 == 0));
-    // Multiplier par 2 chaque élément de la liste.
-    ints = map(ints, i -> i*2);
-    for (Integer i: ints) System.out.println(i);
+        //  Ne retourner que les nombres pairs.
 
-    // TODO - Ne retourner que les Strings contenant au moins 2 lettres t.
-    //strs = ...
-    // TODO - Passer en majuscule toutes les Strings
-    //strs = ...
-    for (String s: strs) System.out.println(s);
+        ints = filter(ints, i -> (0 == i % 2));
 
-  }
+        // Multiplier par 2 chaque élément de la liste.
 
-  /**
-   * Returns a list consisting of the elements of a collection that match the given predicate.
-   * @param c Collection<T>: The input collection
-   * @param p The predicate
-   * @param <T> Type of the element in the collection
-   * @return A list that match the given filter.
-   */
-  private <T> List<T> filter(Collection<T> c, Predicate<T> p) {
+        ints = map(ints, i -> i * 2);
+        for (Integer i : ints) System.out.println(i);
 
-    List<T> result = new ArrayList<>();
+        // Ne retourner que les Strings contenant au moins 2 lettres t.
 
-      //  Ajouter le code ici
+        String exp = "(.*[t].*){2,}";
+        strs = filter(strs, s -> s.matches(exp));
 
-    c.forEach(E ->{
+        //Passer en majuscule toutes les Strings
 
-       if (p.filter(E)) result.add(E);
+        strs = map(strs, String::toUpperCase);
 
-    });
+        for (String s : strs) System.out.println(s);
 
-    // Retourner 'result' contenant les éléments de Collection filtrés par la méthode o.filter().
-    return result;
+    }
 
-  }
+    /**
+     * Returns a list consisting of the elements of a collection that match the given predicate.
+     *
+     * @param c   Collection<T>: The input collection
+     * @param p   The predicate
+     * @param <T> Type of the element in the collection
+     * @return A list that match the given filter.
+     */
+    private <T> List<T> filter(Collection<T> c, Predicate<T> p) {
 
-  /**
-   * Returns a list consisting of the results of applying the given function to the elements of the collection.
-   * @param c Collection<T>: The input collection
-   * @param m The predicate
-   * @param <T> Type of the element in the collection
-   * @param <U> Type of the element in the returned list
-   * @return A list that match the given mapper.
-   */
-  private <T, U> List<U> map(Collection<T> c, Mapper<T, U> m) {
+        List<T> result = new ArrayList<>();
 
-    List<U> result = new ArrayList<>();
-    //  Ajouter le code ici
-      c.forEach(E->result.add(m.map(E)));
-    // Retourner 'result' contenant les éléments de la Collection modifiés par la méthode m.map().
-    return result;
+        //  Ajouter le code ici
 
-  }
+        c.forEach(E -> {
+
+            if (p.filter(E)) result.add(E);
+
+        });
+
+        // Retourner 'result' contenant les éléments de Collection filtrés par la méthode o.filter().
+
+        return result;
+
+    }
+
+    /**
+     * Returns a list consisting of the results of applying the given function to the elements of the collection.
+     *
+     * @param c   Collection<T>: The input collection
+     * @param m   The predicate
+     * @param <T> Type of the element in the collection
+     * @param <U> Type of the element in the returned list
+     * @return A list that match the given mapper.
+     */
+    private <T, U> List<U> map(Collection<T> c, Mapper<T, U> m) {
+
+        List<U> result = new ArrayList<>();
+
+        //  Ajouter le code ici
+
+        c.forEach(E -> result.add(m.map(E)));
+
+        // Retourner 'result' contenant les éléments de la Collection modifiés par la méthode m.map().
+
+        return result;
+
+    }
 
 }
